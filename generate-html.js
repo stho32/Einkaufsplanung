61d6992803d8014d6d56f67d77f9dea597030cf8
Stefan Hoffmann <sthoffmann@onlinehome.de>
2016-07-23 10:45:27 +0200

    statische Webseite statt webserver

---
+++ generate-html.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Wir generieren eine zentrale HTML Seite. Alles wichtige passiert in Javascript.
+ * 
+ */
+
+var rezepte = require("einkauf-lib/rezepte");
+var ejs = require("ejs");
+var fs = require("fs");
+
+ejs.renderFile("views/pages/index.ejs", { rezepte : rezepte.Rezepte }, {}, function(err, html) {
+    if ( err !== null ) {
+        console.log("FEHLER : " + err);
+        return;
+    }
+
+    fs.writeFile("public/index.html", html);
+
+    console.log("index.html wurde erfolgreich erstellt!");
+});
